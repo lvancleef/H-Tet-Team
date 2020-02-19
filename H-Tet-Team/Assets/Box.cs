cc37fbc73a693e15a587153f50ecdb27bc5af2fc
dangpham1 <danggphamm@Gmail.com>
2020-02-19 16:04:47 -0600

    Drag n drop/ Level map

---
+++ H-Tet-Team/Assets/Box.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Box : MonoBehaviour
+{
+    private float startX;
+    private float startY;
+    private bool isBeingHeld = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(isBeingHeld == true)
+        {
+            Vector3 mousePos;
+
+            mousePos = Input.mousePosition;
+            mousePos = Camera.main.ScreenToWorldPoint(mousePos);
+
+            this.gameObject.transform.localPosition = new Vector3(mousePos.x - startX, mousePos.y - startY, 0);
+        }
+    }
+
+    private void OnMouseDown()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector3 mousePos;
+
+            mousePos = Input.mousePosition;
+            mousePos = Camera.main.ScreenToWorldPoint(mousePos);
+
+            startX = mousePos.x - this.transform.localPosition.x;
+            startY = mousePos.y - this.transform.localPosition.y;
+
+            isBeingHeld = true;
+        }
+    }
+
+    private void OnMouseUp()
+    {
+        isBeingHeld = false;
+    }
+}
---
+++ H-Tet-Team/Assets/Scripts/changeLevel.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class changeLevel : MonoBehaviour
+{
+    public void changeScene(string sceneName)
+    {
+        Application.LoadLevel(sceneName);
+    }
+}
