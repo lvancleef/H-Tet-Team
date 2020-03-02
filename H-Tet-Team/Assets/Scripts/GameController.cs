cdfd6de988334b90f96c86ecbcb5186d61fb08fd
dangpham1 <danggphamm@Gmail.com>
2020-03-02 15:53:23 -0600

    Test

---
+++ H-Tet-Team/Assets/Scripts/GameController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class GameController : MonoBehaviour
+{
+    // Data persisted between scenes
+    public static bool PassedLevel2 { get; set; }
+    public static bool PassedLevel3 { get; set; }
+
+    public void passLevel2()
+    {
+        PassedLevel2 = true;
+    }
+
+    public void passLevel3()
+    {
+        PassedLevel3 = true;
+    }
+}
--- H-Tet-Team/Assets/Scripts/changeLevel.cs
+++ H-Tet-Team/Assets/Scripts/changeLevel.cs
@@ -4,8 +4,25 @@ using UnityEngine;
 
 public class changeLevel : MonoBehaviour
 {
+    public GameObject gameController;
     public void changeScene(string sceneName)
     {
-        Application.LoadLevel(sceneName);
+        if (gameController != null)
+        {
+            if(sceneName == "Tutorial 1")
+            {
+                Application.LoadLevel(sceneName);
+            }
+
+            if(GameController.PassedLevel2 == true && sceneName == "Tutorial 2")
+            {
+                Application.LoadLevel(sceneName);
+            }
+
+            else if (GameController.PassedLevel3 == true && sceneName == "Tutorial 3")
+            {
+                Application.LoadLevel(sceneName);
+            }
+        }
     }
 }
